d7f9fcc1180dd1911d4ace767efbf55108554853
Joaquin V <joaquinivl95@gmail.com>
2026-06-07 17:40:26 -0300

    feat: agregue create, update y delete games, y sus respectivos endpoints

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,6 +1,9 @@
 import {
   getAllGames as getAllGamesService,
   getGameById as getGameByIdService,
+  createGame as createGameService,
+  updateGame as updateGameService,
+  deleteGame as deleteGameService,
 } from "./../services/games.service.js";
 
 export const getAllGames = async (req, res) => {
@@ -24,5 +27,42 @@ export const getGameById = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ error: "Error al mostrar juego ingresado por id" });
+  }
+};
+
+export const createGame = async (req, res) => {
+  try {
+    const newGame = await createGameService(req.body);
+    res.status(201).json(newGame);
+  } catch (error) {
+    res.status(500).json({ error: "Error al crear nuevo juego" });
+  }
+};
+
+export const updateGame = async (req, res) => {
+  try {
+    const idGame = parseInt(req.params.id);
+    const update = await updateGameService(idGame, req.body);
+    if (update) {
+      res.status(200).json(update);
+    } else {
+      res.status(404).json({ error: "juego no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error en el intentar actualizar juego" });
+  }
+};
+
+export const deleteGame = async (req, res) => {
+  try {
+    const idGame = parseInt(req.params.id);
+    const deleteGameX = await deleteGameService(idGame);
+    if (deleteGameX) {
+      res.status(200).json(deleteGameX);
+    } else {
+      res.status(404).json({ error: "juego no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: " Error al intentar eliminar le juego" });
   }
 };
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getAllGames, getGameById } from "./../controllers/games.controller.js";
+import {
+  getAllGames,
+  getGameById,
+  createGame,
+  updateGame,
+  deleteGame,
+} from "./../controllers/games.controller.js";
 
 const router = Router();
-
+router.post("/", createGame);
 router.get("/", getAllGames);
 router.get("/:id", getGameById);
-
+router.put("/:id", updateGame);
+router.delete("/:id", deleteGame);
 export default router;
